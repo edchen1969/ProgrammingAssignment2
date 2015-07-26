7277a5346d7a07d7ce8c1cb558cf56fe5d0c22de
edchen1969 <edchen1969@gmail.com>
2015-07-26 15:30:13 -0400

    Updated cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
 ## makeCacheMatrix(x):
 ## This function creates a special "matrix" object that can cache its inverse.
 
-### Version 0.1 (2015-07-26; E. Chen):
+### Version 0.2 (2015-07-26 PM; E. Chen)
+###   
+### Version 0.1 (2015-07-26 AM; E. Chen):
 ###   Intro to R Programming Assignment #2: pseudocode for HW
 
 
@@ -14,7 +16,7 @@ makeCacheMatrix <- function(x = matrix()) {
     InvM <<- NULL
   }
 ## 2: Get the value of the matrix
-  get <- function()
+  get <- function() x
 
 ## 3: Set the value of the inverse matrix
   setInv <- function(solve) InvM <<- solve
@@ -27,17 +29,20 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve(x, ...)
+## cacheSolve(x)
 ##   This function computes the inverse of the special matrix
 ## returned by makeCacheMatrix().
 ##   If the inverse has already been calculated 
 ## (and the matrix has not changed), then cacheSolve should
 ## retrieve the inverse from the cache 
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
 ## Return a matrix that is the inverse of 'x'
 
-  InvM <- x$getInv
+##   Single-argument version of solve() [solve(arg1)] 
+## returns the inverse of arg1 (arg2 when NULL is set to identity matrix) 
+
+  InvM <- x$getInv()
   if(!is.null(InvM)) {
 ## Get cached inverse InvM if it exists
     message("getting cached data")
@@ -46,7 +51,7 @@ cacheSolve <- function(x, ...) {
 ##   If no cached InvM, calculate the inverse 
 ## and set it in the cache using setInv()
   data <- x$get()
-  InvM <- solve(data, ...)
+  InvM <- solve(data)
   x$setInv(InvM)
   InvM  
 }
